5d51c2554782e7c51c80b752b6a975c40e8b5a1b
aiden10 <aiden.ten30@gmail.com>
2024-05-18 05:08:49 -0400

    inputs and drag added to prompt

--- auto.py
+++ auto.py
@@ -8,6 +8,7 @@ use pyautogui to automate the loop of:
 
 after the above is working, try to implement NLP and make more user friendly
 """ 
+import json
 import google.generativeai as genai
 from dotenv import load_dotenv
 import os, shutil
@@ -30,13 +31,24 @@ def delete_screenshots():
         except Exception as e:
             print('Failed to delete %s. Reason: %s' % (file_path, e))
 
+def parse_response(response):
+    data = json.loads(response)
+    X = data["X"]
+    Y = data["Y"]
+    drag = data["Drag"]
+    inputs = data["Inputs"]
+    return X, Y, drag, inputs
+
 load_dotenv()
 API_KEY = os.getenv('API_KEY')
 
 genai.configure(api_key=API_KEY)
 model = genai.GenerativeModel('gemini-pro-vision')
 
+delete_screenshots()
 screenshot = pyautogui.screenshot(path)
+current_screenshot += 1
+
 mouse_x = pyautogui.position()[0]
 mouse_y = pyautogui.position()[1]
 
@@ -45,11 +57,15 @@ if not os.path.isfile(path):
 image = Image.open(path)
 
 prompt = f"""The cursor\'s current position is ({mouse_x}, {mouse_y}). The screen size is 1920 x 1080. Please indicate the 
-             new x and y position for the cursor (where you want to move it to). 
+             new x and y position for the cursor (where you want to move it to). To click and drag, indicate it by writing 'True' in the 'Drag' field.
+             If you would like to type anything please enter your inputs in the 'Inputs' field. Keep in mind however that combinations 
+             will not work and every key press should be separated by commas.
              Expected format: 
                 {{
                   X: *new x position*,
-                  Y: *new y position* 
+                  Y: *new y position*,
+                  Drag: *True/False*,
+                  Inputs: *a, b, c*
                 }}
              """
 response = model.generate_content([prompt, image])
